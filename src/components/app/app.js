d787b1a3c0077adb9ff97f28cd10bcf7b8345631
Arseny <a.grubatch@yandex.ru>
2020-05-06 13:10:32 +0300

    some changes with redirect

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { RegistrationPage, LoginPage, MainPage } from '../pages';
 import './app.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 
 
 const App = () => {
     return (
         <Router>
-            <Route path='/login' exact component={LoginPage}/>
-            <Route path='/reg' exact component={RegistrationPage}/>
-            <Route path='/main' exact component={MainPage}/>
+            <Switch>
+                <Route path='/login' exact component={LoginPage}/>
+                <Route path='/reg' exact component={RegistrationPage}/>
+                <Route path='/main' exact component={MainPage}/>
+                <Redirect to="/login" />
+            </Switch>
         </Router>
     );
 }
